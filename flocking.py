fc0b8c6b29028c8a10cee80c748cede7dbe54fc4
super-cow-powers <dm1306@york.ac.uk>
2022-06-13 14:36:13 +0100

    Flocking func

--- flocking.py
+++ flocking.py
@@ -1,2 +1,32 @@
-def update_flock(flock: list[Robot]) -> list[Robot]:
-    return flock
+def update_flock(robit: Robot) -> Robot:
+    robit = auto_mode(robit)
+    return robit
+
+def auto_mode(robot: Robot) -> Robot:
+    # Autonomous mode
+            if robot.state == RobotState.FORWARDS:
+                left = right = robot.MAX_SPEED
+                if (time.time() - robot.turn_time > 0.5) and any(ir > robot.ir_threshold for ir in robot.ir_readings):
+                    robot.turn_time = time.time()
+                    robot.state = random.choice((RobotState.LEFT, RobotState.RIGHT))
+            elif robot.state == RobotState.BACKWARDS:
+                left = right = -robot.MAX_SPEED
+                robot.turn_time = time.time()
+                robot.state = RobotState.FORWARDS
+            elif robot.state == RobotState.LEFT:
+                left = -robot.MAX_SPEED
+                right = robot.MAX_SPEED
+                if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
+                    robot.turn_time = time.time()
+                    robot.state = RobotState.FORWARDS
+            elif robot.state == RobotState.RIGHT:
+                left = robot.MAX_SPEED
+                right = -robot.MAX_SPEED
+                if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
+                    robot.turn_time = time.time()
+                    robot.state = RobotState.FORWARDS
+            elif robot.state == RobotState.STOP:
+                left = right = 0
+                robot.turn_time = time.time()
+                robot.state = RobotState.FORWARDS
+
--- robot_client.py
+++ robot_client.py
@@ -265,33 +265,7 @@ async def send_commands(robot):
             elif robot.state == RobotState.STOP:
                 left = right = 0
         else:
-            # Autonomous mode
-            if robot.state == RobotState.FORWARDS:
-                left = right = robot.MAX_SPEED
-                if (time.time() - robot.turn_time > 0.5) and any(ir > robot.ir_threshold for ir in robot.ir_readings):
-                    robot.turn_time = time.time()
-                    robot.state = random.choice((RobotState.LEFT, RobotState.RIGHT))
-            elif robot.state == RobotState.BACKWARDS:
-                left = right = -robot.MAX_SPEED
-                robot.turn_time = time.time()
-                robot.state = RobotState.FORWARDS
-            elif robot.state == RobotState.LEFT:
-                left = -robot.MAX_SPEED
-                right = robot.MAX_SPEED
-                if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
-                    robot.turn_time = time.time()
-                    robot.state = RobotState.FORWARDS
-            elif robot.state == RobotState.RIGHT:
-                left = robot.MAX_SPEED
-                right = -robot.MAX_SPEED
-                if time.time() - robot.turn_time > random.uniform(0.5, 1.0):
-                    robot.turn_time = time.time()
-                    robot.state = RobotState.FORWARDS
-            elif robot.state == RobotState.STOP:
-                left = right = 0
-                robot.turn_time = time.time()
-                robot.state = RobotState.FORWARDS
-
+            robot = update_flock(robot)
         message["set_motor_speeds"] = {}
         message["set_motor_speeds"]["left"] = left
         message["set_motor_speeds"]["right"] = right
